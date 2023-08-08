d7fe84ec8766384d21b97ea3a54cde6390882c66
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-08-08 22:04:47 +0200

    Created HTML and JS files
    Added shipping cost & currency conditions and with if...else statements.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+if (location === 'RSA') {
+    shipping = 400;
+    currency = 'R';
+} else if (location === 'NAM') {
+    shipping = 600;
+    currency = 'USD';
+} else {
+    shipping = 800;
+    currency = 'USD';
+}
+
